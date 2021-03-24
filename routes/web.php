d43f4f48cfbd986bab1a9dad40cf6bce604369c9
Cheikh Tidiane KHOULE <ctkhoule@gmail.com>
2021-03-24 15:21:54 +0000

    Ajout contenu de la vue a-propos

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
+// Route  de la page 'A propos'
 Route::get('/a-propos', [App\Http\Controllers\AproposController::class, 'index'])->name('apropos');
